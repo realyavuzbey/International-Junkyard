7921639bf134d165661fd4351c757fbd371b5c77
Mustafa Yavuz Ak <160026656+noyavuzbey@users.noreply.github.com>
2024-02-23 00:48:01 +0300

    Create how-to-loop.php

---
+++ how-to-loop.php
@@ -0,0 +1,18 @@
+<?php
+
+for ($i = 0; $i < 5; $i++) {
+    echo $i;
+}
+
+$num = 0;
+while ($num < 5) {
+    echo $num;
+    $num++;
+}
+
+$colors = array("Kırmızı", "Mavi", "Yeşil");
+foreach ($colors as $color) {
+    echo $color;
+}
+
+?>
